6daf7593eb4509f5027c6fbd9493be6b59100a3f
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-06 17:40:57 +0100

    proxy

--- config.rb
+++ config.rb
@@ -30,7 +30,9 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
+data.flats.each do |member, _|
+  proxy "/flats/#{member}.html", "/flats/show.html", :locals => { :owner => member }
+end
 ###
 # Helpers
 ###
