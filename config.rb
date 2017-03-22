f15d24aa7e493b00b057ba9bb74bfb8df972ed4b
Sibylle de Courcel <sibylle44@gmail.com>
2017-03-22 23:24:24 +0100

    adding data yml

--- config.rb
+++ config.rb
@@ -30,8 +30,8 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-data.flats.each do |member, _|
-  proxy "/flats/#{member}.html", "/flats/show.html", :locals => { :owner => member }
+data.teachers.each do |member, _|
+  proxy "/teachers/#{member}.html", "/teachers/show.html", :locals => { :owner => member }
 end
 ###
 # Helpers
